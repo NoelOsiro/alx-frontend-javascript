af4d4f6347e92f04a6035d70a98715e517f2aa0f
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2024-03-14 21:26:43 +0000

    get list students

---
+++ tests/getListStudents.test.js
@@ -0,0 +1,45 @@
+import getListStudents from '../0-get_list_students';
+
+/**
+ * Retrieve a list of students.
+ * @returns {Array} An array of student objects.
+ */
+describe('getListStudents function', () => {
+  it('returns an array', () => {
+    expect.hasAssertions();
+    const result = getListStudents();
+    expect(Array.isArray(result)).toBe(true);
+  });
+
+  it('returns an array of objects', () => {
+    expect.hasAssertions();
+    const result = getListStudents();
+    expect.assertions(result.length);
+    result.forEach((item) => {
+      expect(typeof item).toBe('object');
+    });
+  });
+
+  it('returns objects with correct attributes', () => {
+    expect.hasAssertions();
+    const result = getListStudents();
+    result.forEach((item) => {
+      expect(item).toMatchObject({
+        id: expect.any(Number),
+        firstName: expect.any(String),
+        location: expect.any(String),
+      });
+    });
+  });
+
+  it('returns objects in the correct order', () => {
+    expect.hasAssertions();
+    const expectedOrder = [
+      { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+      { id: 2, firstName: 'James', location: 'Columbia' },
+      { id: 5, firstName: 'Serena', location: 'San Francisco' },
+    ];
+    const result = getListStudents();
+    expect(result).toStrictEqual(expectedOrder);
+  });
+});
