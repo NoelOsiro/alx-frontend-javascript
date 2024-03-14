16aa06e533d6d081a245ba174a347508281ef811
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2024-03-14 23:07:31 +0000

    Liniting

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,13 +1,14 @@
+// Description: Update student grade by city
 export default function updateStudentGradeByCity(students, city, newGrades) {
-    // check arg is an array
-    if (Object.getPrototypeOf(students) !== Array.prototype) {
-      return [];
-    }
-    if (Object.getPrototypeOf(newGrades) !== Array.prototype) {
-      return [];
-    }
-    return students.filter((student) => student.location === city).map((student) => {
-      const [newGrade] = newGrades.filter((item) => item.studentId === student.id);
-      return { ...student, grade: newGrade ? newGrade.grade : 'N/A' };
-    });
+  // check arg is an array
+  if (Object.getPrototypeOf(students) !== Array.prototype) {
+    return [];
   }
+  if (Object.getPrototypeOf(newGrades) !== Array.prototype) {
+    return [];
+  }
+  return students.filter((student) => student.location === city).map((student) => {
+    const [newGrade] = newGrades.filter((item) => item.studentId === student.id);
+    return { ...student, grade: newGrade ? newGrade.grade : 'N/A' };
+  });
+}
