d6eca28c7097367dbf5a1414d80e6495b0ba3c19
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2024-03-14 21:40:38 +0000

    getListStudentsIds

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,10 @@
+const getListStudentIds = (students) => {
+    // Check if the input is an array
+    if (!Array.isArray(students)) {
+        return [];
+    }
+    
+    // Map over the array of objects and extract the ids
+    return students.map(student => student.id);
+};
+export default getListStudentIds();
