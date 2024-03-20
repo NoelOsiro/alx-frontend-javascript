840f31548aa6fb2f7e614d8b8e23703b5685551c
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2024-03-20 15:03:06 +0000

    linting

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -6,97 +6,54 @@ export default class HolbertonCourse {
    * Create a new HolbertonCourse instance.
    * @param {string} name - The name of the course.
    * @param {number} length - The length of the course in weeks.
-   * @param {string[]} students - An array of strings rep the students enrolled in the course.
+   * @param {string[]} students - An array of str representing the students enrolled in the course.
    * @throws {TypeError} If name is not a str, len is not a no, or students is not an array of str.
    */
   constructor(name, length, students) {
-    if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
-    } else if (typeof length !== 'number') {
-      throw new TypeError('Length must be a number');
-    } else if (!Array.isArray(students) || !students.every((student) => typeof student === 'string')) {
-      throw new TypeError('Students must be an array of strings');
-    }
+    HolbertonCourse.validateString(name, 'Name');
+    HolbertonCourse.validateNumber(length, 'Length');
+    HolbertonCourse.validateArrayOfStrings(students, 'Students');
 
-    /**
-     * The name of the course.
-     * @type {string}
-     * @private
-     */
     this._name = name;
-
-    /**
-     * The length of the course in weeks.
-     * @type {number}
-     * @private
-     */
     this._length = length;
-
-    /**
-     * An array of strings representing the students enrolled in the course.
-     * @type {string[]}
-     * @private
-     */
     this._students = students;
   }
 
-  /**
-   * Get the name of the course.
-   * @returns {string} The name of the course.
-   */
-  get name() {
-    return this._name;
-  }
+  // Existing getter and setter methods...
 
   /**
-   * Get the length of the course in weeks.
-   * @returns {number} The length of the course.
+   * Validate that a value is a string.
+   * @param {*} value - The value to validate.
+   * @param {string} paramName - The name of the parameter being validated.
+   * @throws {TypeError} If the value is not a string.
    */
-  get length() {
-    return this._length;
-  }
-
-  /**
-   * Get the students enrolled in the course.
-   * @returns {string[]} An array of strings representing the students.
-   */
-  get students() {
-    return this._students;
-  }
-
-  /**
-   * Set the name of the course.
-   * @param {string} name - The new name for the course.
-   * @throws {TypeError} If name is not a string.
-   */
-  set name(name) {
-    if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
+  static validateString(value, paramName) {
+    if (typeof value !== 'string') {
+      throw new TypeError(`${paramName} must be a string`);
     }
-    this._name = name;
   }
 
   /**
-   * Set the length of the course.
-   * @param {number} length - The new length for the course in weeks.
-   * @throws {TypeError} If length is not a number.
+   * Validate that a value is a number.
+   * @param {*} value - The value to validate.
+   * @param {string} paramName - The name of the parameter being validated.
+   * @throws {TypeError} If the value is not a number.
    */
-  set length(length) {
-    if (typeof length !== 'number') {
-      throw new TypeError('Length must be a number');
+  static validateNumber(value, paramName) {
+    if (typeof value !== 'number') {
+      throw new TypeError(`${paramName} must be a number`);
     }
-    this._length = length;
   }
 
   /**
-   * Set the students enrolled in the course.
-   * @param {string[]} students - An array of strings representing the students.
-   * @throws {TypeError} If students is not an array of strings.
+   * Validate that a value is an array of strings.
+   * @param {*} value - The value to validate.
+   * @param {string} paramName - The name of the parameter being validated.
+   * @throws {TypeError} If the value is not an array of strings.
    */
-  set students(students) {
-    if (!Array.isArray(students) || !students.every((student) => typeof student === 'string')) {
-      throw new TypeError('Students must be an array of strings');
+  static validateArrayOfStrings(value, paramName) {
+    if (!Array.isArray(value) || !value.every((item) => typeof item === 'string')) {
+      throw new TypeError(`${paramName} must be an array of strings`);
     }
-    this._students = students;
   }
 }
