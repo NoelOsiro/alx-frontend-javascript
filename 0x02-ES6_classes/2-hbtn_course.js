33b7d5b3583d9af93df070bdeb1fb010ad43611e
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2024-03-20 15:06:25 +0000

    linting

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,59 +1,48 @@
-/**
- * Represents a course offered by ALX School.
- */
 export default class HolbertonCourse {
-  /**
-   * Create a new HolbertonCourse instance.
-   * @param {string} name - The name of the course.
-   * @param {number} length - The length of the course in weeks.
-   * @param {string[]} students - An array of str representing the students enrolled in the course.
-   * @throws {TypeError} If name is not a str, len is not a no, or students is not an array of str.
-   */
   constructor(name, length, students) {
-    HolbertonCourse.validateString(name, 'Name');
-    HolbertonCourse.validateNumber(length, 'Length');
-    HolbertonCourse.validateArrayOfStrings(students, 'Students');
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    } else if (!Array.isArray(students)) {
+      throw new TypeError('Students must be an array of strings');
+    }
 
     this._name = name;
     this._length = length;
     this._students = students;
   }
 
-  // Existing getter and setter methods...
-
-  /**
-   * Validate that a value is a string.
-   * @param {*} value - The value to validate.
-   * @param {string} paramName - The name of the parameter being validated.
-   * @throws {TypeError} If the value is not a string.
-   */
-  static validateString(value, paramName) {
-    if (typeof value !== 'string') {
-      throw new TypeError(`${paramName} must be a string`);
-    }
+  get name() {
+    return this._name;
   }
 
-  /**
-   * Validate that a value is a number.
-   * @param {*} value - The value to validate.
-   * @param {string} paramName - The name of the parameter being validated.
-   * @throws {TypeError} If the value is not a number.
-   */
-  static validateNumber(value, paramName) {
-    if (typeof value !== 'number') {
-      throw new TypeError(`${paramName} must be a number`);
-    }
+  get length() {
+    return this._length;
   }
 
-  /**
-   * Validate that a value is an array of strings.
-   * @param {*} value - The value to validate.
-   * @param {string} paramName - The name of the parameter being validated.
-   * @throws {TypeError} If the value is not an array of strings.
-   */
-  static validateArrayOfStrings(value, paramName) {
-    if (!Array.isArray(value) || !value.every((item) => typeof item === 'string')) {
-      throw new TypeError(`${paramName} must be an array of strings`);
+  get students() {
+    return this._students;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
     }
+    this._name = name;
+  }
+
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = length;
+  }
+
+  set students(students) {
+    if (!Array.isArray(students)) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = students;
   }
 }
