d31ccd664d03d63013abcd7f16edd76b50c316c3
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2024-03-20 14:34:40 +0000

    linting

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,17 +1,5 @@
-/**
- * Represents a classroom with a maximum number of students.
- */
 export default class ClassRoom {
-  /**
-   * Create a new ClassRoom instance.
-   * @param {number} maxStudentsSize - The maximum number of students allowed in the classroom.
-   */
-  constructor(maxStudentsSize) {
-    /**
-     * The maximum number of students allowed in the classroom.
-     * @type {number}
-     * @private
-     */
-    this.maxStudentsSize = maxStudentsSize;
+    constructor(maxStudentsSize) {
+      this._maxStudentsSize = maxStudentsSize;
+    }
   }
-}
